f4a233c0c7fa4d7b345c5336babc690278310224
riversideholdings <107801528+riversideholdings@users.noreply.github.com>
2022-06-19 23:39:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ var year  = d.getFullYear();
 var dateTag = document.getElementById("yearDate");
 dateTag.innerHTML =  `Management panel &copy; ${year} - Riverside Holdings.` ;
 
+
+//SCROLL TO FUNCTION
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.body.style = "transition: ease-in 0.3s; behavior: smooth;";
+  document.documentElement.scrollTop = 0;
+}
+
+
