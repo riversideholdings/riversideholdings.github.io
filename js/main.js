6e1d6d611a20d9718eb211aca0a45ad0b84c44e0
riversideholdings <107801528+riversideholdings@users.noreply.github.com>
2022-06-20 15:33:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -28,3 +28,15 @@ function topFunction() {
 }
 
 
+///side navigation
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "300px";
+
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+
+}
