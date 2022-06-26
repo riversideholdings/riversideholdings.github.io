bbf956a7913aa310c7e1ec69b2ec0e4e060a2606
riversideholdings <107801528+riversideholdings@users.noreply.github.com>
2022-06-26 21:59:12 +0200

    Add files via upload

---
+++ js/requestprod.js
@@ -0,0 +1,99 @@
+//global var
+var productDiv = document.getElementById("prod_List");
+
+//https://docs.google.com/spreadsheets/d/1fpQMtNFWrPtuwMH2rtnfGCea7vmwl6CAwnGZXVcYrOU/edit?usp=sharing
+const sheetId2 = '1fpQMtNFWrPtuwMH2rtnfGCea7vmwl6CAwnGZXVcYrOU';
+const base2 = `https://docs.google.com/spreadsheets/d/${sheetId2}/gviz/tq?`;
+const sheetName2 = 'Products - info';
+const query2 = encodeURIComponent('Select *')
+const url2 = `${base2}&sheet=${sheetName2}&tq=${query2}`
+
+//DOM function lisatener
+const client = []
+document.addEventListener('DOMContentLoaded', init)
+ 
+/*INT FUNCTION TO PROCCESS ***This function fetches data from the google sheet detailed above
+after the data is fethced in the function, it is then changed from an unstructures array 
+to a structured array to use in other functoons****/
+function init(){
+    fetch(url2)
+    .then(res => res.text())
+    .then(rep => {
+        //Remove additional text and extract only JSON:
+        const jsonData2 = JSON.parse(rep.substring(47).slice(0, -2));
+        //rows of the data retrieved
+        const prodArr = jsonData2.table.rows;
+        //=============LOG TO CONSOLE========================//
+        //console.log(jsonData2);
+        //console.log(prodArr);
+
+        let nw = [ ];
+
+         //check selected values in client add form /existing client
+         var exiselect = document.getElementById("exiRecipient");
+         var recpAddress = document.getElementById("Recipient Address");
+         var clCount = document.getElementById("clientCount");
+
+
+         
+        for (let i = 1; i < prodArr.length; i++) {
+      
+          var Prd_Code = prodArr[i].c[0].v;
+          var Product =	prodArr[i].c[1].v;
+          var SellingP =	prodArr[i].c[2].v;
+          var WaterpreneurP =	prodArr[i].c[3].v;
+          var wholesaleP = prodArr[i].c[4].v;
+          var packSize = prodArr[i].c[5].v;
+          var PalletSize= prodArr[i].c[6].v;
+          var imgUrl = prodArr[i].c[7].v;
+
+          
+
+          //contruction a proper structured array of the data
+          nw.push({
+                "Prd_Code":	Prd_Code,
+                "Product": Product,	
+                "Selling_Price": SellingP,	
+                "Waterpreneur_Price": WaterpreneurP,	
+                "wholesale_Price": wholesaleP,
+                "Pack_Size": packSize,
+                "Pallet_Size": PalletSize,
+                "img_url": imgUrl
+            });
+
+           
+
+            try{
+              existcl.innerHTML += `<option>${Name}</option>`;
+              
+            }
+            catch{
+                //
+            }
+            
+            try{
+            productDiv.innerHTML += `<div class="col-sm-6">
+            <div class="card" style="width: 35rem; font-family:'Times New Roman', Times, serif;">
+            <img src="${imgUrl}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title"> ${Product}</h4>
+                <hr>
+                <h5 class="card-text"><b>Prices:</b></h5>
+                <p>Wholesale Price: <b>${wholesaleP}</b></p>
+                <p>Retail Price: <b>${SellingP}</b></p>
+                <p>Waterpreneur Price: <b>${WaterpreneurP}</b></p>
+                <hr>
+                <button type="button" class="btn btn-primary">Details</button>
+            </div>
+          </div></div>`;
+
+            }
+            catch{
+                //
+            }
+        }
+
+        console.log(nw);
+
+    })        
+}
